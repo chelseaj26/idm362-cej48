d3d31445216fe63019e2208672ac43ccf6be2346
Chelsea <cej48@drexel.edu>
2021-03-02 00:24:07 -0500

    added table view for home screen

--- idm362-cej48/ViewController.swift
+++ idm362-cej48/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var tableView: UITableView!
+    
+    let place = [
+    "Home",
+    "Public",
+    "Grocery Store",
+    "Work",
+    "+ Add New Place"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("tapped")
+    }
+    
+}
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return place.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier:"cell", for: indexPath)
+        
+        cell.textLabel?.text = place[indexPath.row]
+    
+        return cell
+    }
+}
